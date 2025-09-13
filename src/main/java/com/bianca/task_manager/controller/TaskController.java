50b89855091f365e79d4566344879ddf4ceaca7a
Bianca Pacífico <bi.marcal22@gmail.com>
2025-09-13 10:23:54 -0300

    create task controller

---
+++ src/main/java/com/bianca/task_manager/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.bianca.task_manager.controller;
+
+import com.bianca.task_manager.entity.Task;
+import com.bianca.task_manager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private TaskService taskService;
+
+    //constructor injection
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    List<Task> create(@RequestBody Task task) {
+        return taskService.create(task);
+    }
+
+    @GetMapping
+    List<Task> listAll() {
+        return taskService.listAll();
+    }
+
+    @PutMapping
+    List<Task> update(@RequestBody Task task) {
+        return taskService.update(task);
+    }
+
+    @DeleteMapping("{id}")
+    List<Task> delete(@PathVariable("id") Long id) {
+        return taskService.delete(id);
+    }
+}
